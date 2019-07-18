b63c655c98724243f098e418ec65b6da6ec5c52a
Gabriel Gomez <g.a.gomez@gmail.com>
2019-07-17 21:01:03 -0300

    Comentarios actualizados

--- digito_verificador.php
+++ digito_verificador.php
@@ -9,13 +9,13 @@ $periodo = $_REQUEST['periodo'];
 
 // Armamos el codigo completo a procesar
 $codigo_proceso = $cuit.$periodo;
-// Sumamos las posiciones impares
+// Sumamos las posiciones impares y las multiplicamos por 3
 $codigo_impares = (($codigo_proceso[0] + $codigo_proceso[2] + $codigo_proceso[4] + $codigo_proceso[6] + $codigo_proceso[8] + $codigo_proceso[10] + $codigo_proceso[12] + $codigo_proceso[14] + $codigo_proceso[16]) * 3);
 // Sumamos las posiciones pares
 $codigo_pares = $codigo_proceso[1] + $codigo_proceso[3] + $codigo_proceso[5] + $codigo_proceso[7] + $codigo_proceso[9] + $codigo_proceso[11] + $codigo_proceso[13] + $codigo_proceso[15];
-// Obtenemos el código mayor para su posterior comparación 
+// Obtenemos el código menor para su posterior comparación 
 $codigo_menor = ($codigo_impares + $codigo_pares);
-// Obtenemos el código menor. Acá utilizamos la funcion ceil() para redondeardo (FIXME)
+// Obtenemos el código mayor. Acá utilizamos la funcion ceil() para redondearlo (FIXME)
 $codigo_mayor = ceil($codigo_menor / 10) * 10;
 
 // Si menor es igual a mayor, significa que la diferencia es 10. Por lo que el digito verificador es 9 (10 -1)
